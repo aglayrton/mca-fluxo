c77361255687f8fee735f8bb31502186088003db
Gustavo Cavalcante Gomes <130493202+Gustavo-PGM@users.noreply.github.com>
2024-11-29 20:51:04 -0300

    feat: salvar dias anteriores

--- fluxo/views.py
+++ fluxo/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets, status, mixins
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Sum
-from django.utils import timezone
-from datetime import date
+from django.utils.timezone import timezone, localtime
+from datetime import date, timedelta
 import logging
 from .models import FluxoAgua, ConsumoDiario
 from .serializers import FluxoAguaSerializer, ConsumoDiarioSerializer
@@ -17,21 +17,34 @@ class FluxoViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Gene
         today = timezone.localtime().date()
         return FluxoAgua.objects.filter(data_hora__date=today).order_by('-data_hora')
 
-from django.utils.timezone import localtime
 
 class ConsumoDiario(APIView):
     def get(self, request):
-        now = localtime()
+        now = timezone.localtime()
         hoje = now.date()
+        ontem = hoje - timedelta(days=1)
+
+        if not ConsumoDiario.objects.filter(data=ontem).exists():
+            consumo_ontem = FluxoAgua.objects.filter(data_hora__date=ontem).aggregate(total=Sum('consumo_diario'))['total'] or 0
+            ultima_hora_ontem = (
+                FluxoAgua.objects.filter(data_hora__date=ontem)
+                .order_by('-data_hora')
+                .first()
+            )
+            hora_ultima_alteracao = (
+                ultima_hora_ontem.data_hora.strftime('%H:%M:%S') if ultima_hora_ontem else None
+            )
+            ConsumoDiario.objects.create(
+                data=ontem,
+                consumo_total=consumo_ontem,
+                hora=hora_ultima_alteracao
+            )
         consumo_total = FluxoAgua.objects.filter(data_hora__date=hoje).aggregate(total=Sum('consumo_diario'))['total'] or 0
         ultima_modificacao = FluxoAgua.objects.filter(data_hora__date=hoje).order_by('-data_hora').first()
-
-        ultima_hora = localtime(ultima_modificacao.data_hora).strftime('%H:%M:%S') if ultima_modificacao else None
+        ultima_hora = ultima_modificacao.data_hora.strftime('%H:%M:%S') if ultima_modificacao else None
 
         return Response({
             "data": now.strftime('%d/%m/%Y'),
             "hora": ultima_hora or "Sem dados",
             "consumo_diario": consumo_total
         }, status=status.HTTP_200_OK)
-
-
