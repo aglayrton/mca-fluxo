4d8100386dda6408596aa7bfe4b41609037b94fa
Gustavo Cavalcante Gomes <130493202+Gustavo-PGM@users.noreply.github.com>
2024-11-29 20:33:48 -0300

    fix: Hora CD corrigida

--- views.py
+++ views.py
@@ -17,17 +17,21 @@ class FluxoViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Gene
         today = timezone.localtime().date()
         return FluxoAgua.objects.filter(data_hora__date=today).order_by('-data_hora')
 
+from django.utils.timezone import localtime
+
 class ConsumoDiario(APIView):
     def get(self, request):
-        now = timezone.localtime()
+        now = localtime()
         hoje = now.date()
         consumo_total = FluxoAgua.objects.filter(data_hora__date=hoje).aggregate(total=Sum('consumo_diario'))['total'] or 0
         ultima_modificacao = FluxoAgua.objects.filter(data_hora__date=hoje).order_by('-data_hora').first()
-        ultima_hora = ultima_modificacao.data_hora.strftime('%H:%M:%S') if ultima_modificacao else None
+
+        ultima_hora = localtime(ultima_modificacao.data_hora).strftime('%H:%M:%S') if ultima_modificacao else None
 
         return Response({
             "data": now.strftime('%d/%m/%Y'),
             "hora": ultima_hora or "Sem dados",
             "consumo_diario": consumo_total
         }, status=status.HTTP_200_OK)
+
 
